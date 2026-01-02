5119a6655fc4f79015ca72bfe032e51ad7372cf3
Yasaman Amou Jafary <yasamanamoujafary@gmail.com>
2026-01-02 19:09:46 +0000

    feat: part 1 without system calls vread and vwrite is done and tested

--- CA5/codes/cpt.d
+++
@@ -1,2 +0,0 @@
-cpt.o: cpt.c /usr/include/stdc-predef.h types.h defs.h memlayout.h mmu.h \
- cpt.h param.h spinlock.h
--- CA5/codes/test_duplicate.d
+++
@@ -1,2 +0,0 @@
-test_duplicate.o: test_duplicate.c /usr/include/stdc-predef.h types.h \
- stat.h user.h
